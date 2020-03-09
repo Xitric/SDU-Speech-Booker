4469e0b26fde7bda6d1e132c1226e18be809dbfb
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2020-03-09 10:13:31 +0100

    Set up some dummy functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,24 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import {dialogflow, Suggestions} from 'actions-on-google'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Instantiate DialogFlow client
+interface ItemData {
+    item: object | undefined | string
+}
+
+const app = dialogflow<ItemData, {}>({debug: true})
+
+app.intent('out of items', (conv, {item}) => {
+    //Store item in user data?
+    conv.data.item = item
+    conv.ask(`Would you like to add ${item} to your shopping list?`)
+    conv.ask(new Suggestions('Yes', 'No'))
+})
+
+app.intent('discounts - yes', (conv) => {
+    //Retrieve item from user data?
+    conv.close(`Sorry, no discounts available on ${conv.data.item}!`)
+})
+
+// Handle HTTPS POST requests
+export const fulfillment = functions.https.onRequest(app)
